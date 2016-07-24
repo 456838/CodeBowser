c7231b7f3985941f8d7482beacd6199581646552
js456838 <563185579@qq.com>
2016-07-24 14:02:45 +0800

    20160724 first commit project Create 代码浏览器 代码阅读器

---
+++ app/src/main/java/salton123/com/codebrowser/handler/DocumentHandler.java
@@ -0,0 +1,10 @@
+package salton123.com.codebrowser.handler;
+
+public interface DocumentHandler {
+	public String getFileExtension();
+	public String getFileMimeType();
+	public String getFilePrettifyClass();
+	public String getFileFormattedString(String fileString);
+	public String getFileScriptFiles();
+	
+}
---
+++ app/src/main/java/salton123/com/codebrowser/handler/JavaDocumentHandler.java
@@ -0,0 +1,33 @@
+package salton123.com.codebrowser.handler;
+
+import android.text.TextUtils;
+
+public class JavaDocumentHandler implements DocumentHandler {
+
+	@Override
+	public String getFileExtension() {
+		return ".java";
+	}
+
+	@Override
+	public String getFileFormattedString(String fileString) {
+		return TextUtils.htmlEncode(fileString).replace("\n", "<br>");
+	}
+
+	@Override
+	public String getFileMimeType() {
+		return "text/html";
+	}
+
+	@Override
+	public String getFilePrettifyClass() {
+		
+		return "prettyprint";
+	}
+
+	@Override
+	public String getFileScriptFiles() {
+		return "";
+	}
+
+}
